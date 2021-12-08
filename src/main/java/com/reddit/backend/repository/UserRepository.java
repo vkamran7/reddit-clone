effb9442e368b600c41b0477349bbdfc84b5991e
vkamran7 <vkamran7@gmail.com>
2021-12-08 10:59:49 +0400

    adding user repo

---
+++ src/main/java/com/reddit/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.reddit.backend.repository;
+
+import com.reddit.backend.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
