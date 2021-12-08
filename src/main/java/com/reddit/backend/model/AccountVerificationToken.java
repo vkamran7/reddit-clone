9f7c8e18df430b76f632fed167257455c9028c58
vkamran7 <vkamran7@gmail.com>
2021-12-08 10:57:47 +0400

    adding account verification and notification email models

---
+++ src/main/java/com/reddit/backend/model/AccountVerificationToken.java
@@ -0,0 +1,26 @@
+package com.reddit.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "token")
+@Entity
+public class AccountVerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expirationDate;
+}
