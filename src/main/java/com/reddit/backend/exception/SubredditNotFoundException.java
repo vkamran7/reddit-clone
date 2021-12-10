b00123d6aa247d7498254f0794f9a60e8f4da04f
vkamran7 <vkamran7@gmail.com>
2021-12-10 13:44:43 +0400

    adding SubredditNotFoundException class

---
+++ src/main/java/com/reddit/backend/exception/SubredditNotFoundException.java
@@ -0,0 +1,7 @@
+package com.reddit.backend.exception;
+
+public class SubredditNotFoundException extends RuntimeException {
+    public SubredditNotFoundException(String message) {
+        super(message);
+    }
+}
