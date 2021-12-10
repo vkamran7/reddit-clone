99f5ba6cb9545788513006b7212a8f3ad94abed3
vkamran7 <vkamran7@gmail.com>
2021-12-10 13:43:35 +0400

    adding SubredditDTO

---
+++ src/main/java/com/reddit/backend/dto/SubredditDTO.java
@@ -0,0 +1,19 @@
+package com.reddit.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubredditDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer postCount;
+    private Integer memberCount;
+}
