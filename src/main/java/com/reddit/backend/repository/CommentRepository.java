a094da08f6b6c99019ea2bf66ab451fa8699fbcc
vkamran7 <vkamran7@gmail.com>
2021-12-14 17:21:20 +0400

    update comment repository

--- src/main/java/com/reddit/backend/repository/CommentRepository.java
+++ src/main/java/com/reddit/backend/repository/CommentRepository.java
@@ -3,11 +3,11 @@ package com.reddit.backend.repository;
 import com.reddit.backend.model.Comment;
 import com.reddit.backend.model.Post;
 import com.reddit.backend.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
 
     List<Comment> findAllByUser(User user);
