fb7663228305720755a6cd1eb3eac5d473aa76df
vkamran7 <vkamran7@gmail.com>
2021-12-14 17:19:50 +0400

    adding comment dto classes

---
+++ src/main/java/com/reddit/backend/dto/CommentRequest.java
@@ -0,0 +1,18 @@
+package com.reddit.backend.dto;
+
+
+import com.reddit.backend.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentRequest {
+    private Long id;
+    private String text;
+    private Post post;
+}
---
+++ src/main/java/com/reddit/backend/dto/CommentResponse.java
@@ -0,0 +1,22 @@
+package com.reddit.backend.dto;
+
+import com.reddit.backend.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponse {
+
+    private Long id;
+    private String text;
+    private Long postId;
+    private Instant creationDate;
+    private User user;
+}
