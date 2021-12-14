ec8f21458e0853df58a332e2b33ddfa180ddb90e
vkamran7 <vkamran7@gmail.com>
2021-12-14 22:37:53 +0400

    adding vote dto

---
+++ src/main/java/com/reddit/backend/dto/VoteDTO.java
@@ -0,0 +1,14 @@
+package com.reddit.backend.dto;
+
+import com.reddit.backend.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDTO {
+    private VoteType voteType;
+    private Long id;
+}
